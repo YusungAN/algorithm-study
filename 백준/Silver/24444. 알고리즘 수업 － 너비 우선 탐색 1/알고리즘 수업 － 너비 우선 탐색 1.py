0efd5a28447b9a10c1cae7d4fda31f025815fce5
YusungAN <52990109+YusungAN@users.noreply.github.com>
2022-09-25 01:14:27 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 596 ms, Memory: 62400 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,31 @@
+import sys
+from collections import defaultdict
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split(' '))
+visited = [False]*(n+1)
+dic = defaultdict(list)
+q = deque([])
+for i in range(m):
+    a, b = map(int, input().split(' '))
+    dic[a].append(b)
+    dic[b].append(a)
+res = [0]*(n+1)
+visited[r] = True
+q.append(r)
+cnt = 1
+res[r] = cnt
+while len(q) > 0:
+    u = q.popleft()
+    dic[u].sort()
+    for i in dic[u]:
+        if not visited[i]:
+            visited[i] = True
+            q.append(i)
+            cnt += 1
+            res[i] = cnt
+
+for i in res[1:]:
+    print(i)
