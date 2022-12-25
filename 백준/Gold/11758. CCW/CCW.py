7ac7c387ffd7bc3120c4be91c6b83a02e15d7fc7
YusungAN <52990109+YusungAN@users.noreply.github.com>
2022-12-26 02:18:23 +0900

    [Gold V] Title: CCW, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+x1, y1 = map(int, input().split(' '))
+x2, y2 = map(int, input().split(' '))
+x3, y3 = map(int, input().split(' '))
+
+
+res = (x2-x1)*(y3-y1)-(y2-y1)*(x3-x1)
+
+if res > 0:
+    print('1')
+elif res == 0:
+    print('0')
+else:
+    print('-1')
