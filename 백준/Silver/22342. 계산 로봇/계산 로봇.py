5da13f1cf02b65592418afc5e059ff3d85d177e7
YusungAN <52990109+YusungAN@users.noreply.github.com>
2023-05-07 01:11:51 +0900

    [Silver I] Title: 계산 로봇, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/22342. 계산 로봇/계산 로봇.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+m, n = map(int, input().split(' '))
+li = [list(map(int, list(input().rstrip()))) for _ in range(m)]
+dp = [[0]*n for _ in range(m)]
+
+_max = 0
+for i in range(m):
+    dp[i][0] = li[i][0]
+    _max = max(_max, li[i][0])
+
+for i in range(1, n):
+    for j in range(m):
+        dp[j][i] = max(0 if j == 0 else dp[j-1][i-1], dp[j][i-1], 0 if j == m-1 else dp[j+1][i-1])
+        _max = max(_max, dp[j][i])
+        dp[j][i] += li[j][i]
+
+print(_max)
