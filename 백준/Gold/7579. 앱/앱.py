d7d1124d4cc0c6e7ee32fd44a5b6f46811e3a638
YusungAN <52990109+YusungAN@users.noreply.github.com>
2024-01-16 02:42:17 +0900

    [Gold III] Title: 앱, Time: 136 ms, Memory: 116228 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -2,21 +2,21 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split(' '))
-mems = list(map(int, input().split(' ')))
-costs = list(map(int, input().split(' ')))
-max_cost = sum(costs)
-dp = [[0]*(max_cost+1) for _ in range(n+1)]
-res = max_cost
+n, m = map(int, input().split())
+mem = list(map(int, input().split()))
+cost = list(map(int, input().split()))
+cost_sum = sum(cost)
 
-for i in range(1, n+1):
-    for j in range(1, max_cost+1):
-        if j < costs[i-1]:
-            dp[i][j] = dp[i-1][j]
-        else:
-            dp[i][j] = max(mems[i-1]+dp[i-1][j-costs[i-1]], dp[i-1][j])
+dp = [[0]*(cost_sum+1) for _ in range(n)]
 
-        if dp[i][j] >= m:
-            res = min(res, j)
+for i in range(n):
+    for j in range(cost_sum+1):
+        if j - cost[i] >= 0:
+            dp[i][j] = max(dp[i-1][j-cost[i]]+mem[i], dp[i][j])
+        dp[i][j] = max(dp[i][j], dp[i-1][j])
 
-print(res)
+
+for i in range(cost_sum+1):
+    if dp[n-1][i] >= m:
+        print(i)
+        break
